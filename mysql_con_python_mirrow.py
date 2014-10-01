7c550142f1cd5c975d08febd75343411e5fe42c2
Jay Prakash Singh <jp213@ymail.com>
2014-10-01 12:29:32 +0530

    Update mysql_con_python_mirrow.py

--- mysql_con_python_mirrow.py
+++ mysql_con_python_mirrow.py
@@ -6,7 +6,7 @@ import MySQLdb
 
 
 def supermain():
-    db = MySQLdb.connect("localhost","root","6Tresxcvbhy")
+    db = MySQLdb.connect("localhost","root","XXXX")
     cursor = db.cursor()
 
     sql = "create database if not exists  snapdeal"
